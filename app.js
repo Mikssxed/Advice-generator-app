0dea0170068d1590807929d6bae055cf96ba9ca6
Mikssxed <112573426+Mikssxed@users.noreply.github.com>
2022-10-30 20:39:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const adviceId = document.querySelector('h1');
+const advice = document.querySelector('p');
+const button = document.querySelector('button')
+
+const getAdvice = () => {
+    fetch('https://api.adviceslip.com/advice').then(response => {
+    return response.json();
+  }).then(showAdvice => {
+    const adviceObj = showAdvice.slip;
+    advice.innerHTML = `"${adviceObj.advice}"`;
+    adviceId.innerHTML = `advice #${adviceObj.id}`
+  })
+}
+
+window.onload = () => {
+    getAdvice();
+}
+
+button.addEventListener('click', getAdvice)
